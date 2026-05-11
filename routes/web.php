4a4781667d7aff55980a05e805b94e6d4cbf2086
resparagoza559817-cpu <r.esparagoza.559817@umindanao.edu.ph>
2026-05-11 13:04:29 +0800

    images

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('/products');
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
